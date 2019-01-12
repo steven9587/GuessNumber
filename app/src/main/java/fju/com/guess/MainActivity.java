180162ea9ff61e3743e9214b549d90ed2941a47d
steven9587 <steven9587@gmail.com>
2019-01-12 16:09:56 +0800

    2019/01/12 first commit

--- app/src/main/java/fju/com/guess/MainActivity.java
+++ app/src/main/java/fju/com/guess/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private int finish;
     private int number;
     private int count;
+    private TextView layoutTimes;
+    private TextView layoutStart;
+    private TextView layoutFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,60 +35,88 @@ public class MainActivity extends AppCompatActivity {
         finish = 100;
         //產生secret
         Random r = new Random();
-        secret = r.nextInt(100)+1;
+        secret = r.nextInt(100) + 1;
+        layoutTimes = findViewById(R.id.times);
+        layoutTimes.setText(count + " ");
         layoutSecret = findViewById(R.id.secret);
-        layoutSecret.setText(secret+" ");
+        layoutSecret.setText(secret + " ");
+        layoutStart = findViewById(R.id.start);
+        layoutStart.setText(start + " ");
+        layoutFinish = findViewById(R.id.finish);
+        layoutFinish.setText(finish + " ");
         layoutNumber = findViewById(R.id.number);
     }
-    public void send(View view){
+
+    public void send(View view) {
         number = Integer.parseInt(layoutNumber.getText().toString());
         //Secret > GuessNumner (number~finish)
-        if (secret > number){
-            start = number;
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("Message")
-                    .setMessage(start+"  to "+finish)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            layoutNumber.setText("");
-                        }
-                    })
-                    .show();
-            count++;
-        }
-        //Secret = GuessNumner
-        if (secret == number){
-        new AlertDialog.Builder(MainActivity.this)
-                .setTitle("Message")
-                .setMessage("Yes, the number is "+secret)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        layoutNumber.setText("");
-                    }
-                })
-                .show();
-            count++;
-                if (count < 4){
-                Intent excellent = new Intent(this,ExcellentActivity.class);
-                startActivity(excellent);
-             }
+        if (number > start && number < finish) {
+            if (secret > number) {
+                count++;
+                start = number;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Message")
+                        .setMessage(start + "  to " + finish)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                layoutNumber.setText("");
+                                layoutStart.setText(start + " ");
+                                layoutTimes.setText(count + " ");
+                            }
+                        })
+                        .show();
             }
-        //Secret < GuessNumner
-        if (secret < number){
-            finish = number;
+            //Secret = GuessNumner
+            if (secret == number) {
+                count++;
+                if (count < 4) {
+                    Intent excellent = new Intent(this, ExcellentActivity.class);
+                    startActivity(excellent);
+                } else {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Message")
+                            .setMessage("Yes, the number is " + secret)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    layoutNumber.setText("");
+                                    layoutTimes.setText(count + " ");
+                                }
+                            })
+                            .show();
+                }
+            }
+            //Secret < GuessNumner
+            if (secret < number) {
+                count++;
+                finish = number;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Message")
+                        .setMessage(start + "  to " + finish)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                layoutNumber.setText("");
+                                layoutFinish.setText(finish + " ");
+                                layoutTimes.setText(count + " ");
+                            }
+                        })
+                        .show();
+            }
+        } else {
+            count++;
             new AlertDialog.Builder(MainActivity.this)
                     .setTitle("Message")
-                    .setMessage(start+"  to "+finish)
+                    .setMessage("請輸入" + start + "到" + finish + "之間的數")
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             layoutNumber.setText("");
+                            layoutTimes.setText(count + " ");
                         }
                     })
                     .show();
-            count++;
         }
     }
 }
